40548dd0d39488c97ba1a99ccf1bbc1555ec4036
Titan Cassini <58642686+titancassini@users.noreply.github.com>
2023-12-03 20:42:24 +0000

    Create ParticipatoryPlanning.sol

---
+++ ParticipatoryPlanning.sol
@@ -0,0 +1,60 @@
+// Import necessary libraries (for example, SafeMath for secure mathematical operations)
+// import "github.com/OpenZeppelin/contracts/math/SafeMath.sol";
+
+contract ParticipatoryPlanning {
+    // Define data structures to store proposals and prices
+    mapping(address => uint256) public indicativePrices;
+    mapping(address => uint256) public consumerProposals;
+    mapping(address => uint256) public workerProposals;
+
+    // Define roles for Worker Councils, Consumer Councils, and Iteration Facilitation Board
+    address public workerCouncils;
+    address public consumerCouncils;
+    address public iterationFacilitationBoard;
+
+    // Define events for proposal submission and approval
+    event ProposalSubmitted(address indexed submitter, uint256 amount, string proposalType);
+    event ProposalApproved(address indexed approver, address indexed submitter, string proposalType);
+
+    // Modifier to ensure only authorized councils can perform certain actions
+    modifier onlyCouncils() {
+        require(msg.sender == workerCouncils || msg.sender == consumerCouncils || msg.sender == iterationFacilitationBoard, "Unauthorized");
+        _;
+    }
+
+    // Function for Worker Councils to submit production proposals
+    function submitWorkerProposal(uint256 amount) external onlyCouncils {
+        workerProposals[msg.sender] = amount;
+        emit ProposalSubmitted(msg.sender, amount, "Worker");
+    }
+
+    // Function for Consumer Councils to submit consumption proposals
+    function submitConsumerProposal(uint256 amount) external onlyCouncils {
+        consumerProposals[msg.sender] = amount;
+        emit ProposalSubmitted(msg.sender, amount, "Consumer");
+    }
+
+    // Function for Iteration Facilitation Board to update indicative prices
+    function updateIndicativePrices(address asset, uint256 newPrice) external onlyCouncils {
+        indicativePrices[asset] = newPrice;
+    }
+
+    // Function for Councils to approve proposals based on social benefit-to-cost ratios
+    function approveProposal(address submitter, string memory proposalType) external onlyCouncils {
+        require(submitter != address(0), "Invalid submitter address");
+
+        if (keccak256(abi.encodePacked(proposalType)) == keccak256(abi.encodePacked("Consumer"))) {
+            // Logic for evaluating and approving consumer proposals
+            // ...
+
+        } else if (keccak256(abi.encodePacked(proposalType)) == keccak256(abi.encodePacked("Worker"))) {
+            // Logic for evaluating and approving worker proposals
+            // ...
+
+        } else {
+            revert("Invalid proposal type");
+        }
+
+        emit ProposalApproved(msg.sender, submitter, proposalType);
+    }
+}
